5bf7dc097a0b42d27bbe83ee9e75b0ccd8581918
root <han99728@gmail.com>
2024-05-06 09:15:41 +0000

    mammtho

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -42,7 +42,6 @@ process_execute (const char *file_name)
   char *saveptr;
   file_name = strtok_r(file_name, " ", &saveptr);
 
-
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
