219cf4db16eda676070a8e2087cbfbe6c4f458fe
ChanHHaeng <dlcksgod2325@gmail.com>
2024-05-10 22:33:44 +0000

    argument parsing, writing, handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,17 +8,18 @@
 #include "threads/vaddr.h"
 #include "userprog/pagedir.h"
 #include "devices/shutdown.h"
+#include "filesys/file.h"
 
-static pid_t exec (const char *);
-static void halt (void);
-static void exit (int);
-static int write (int, const void *, unsigned);
+static pid_t exec(const char *);
+static void halt(void);
+static void exit(int);
+static int write(int, const void *, unsigned);
 
 static void syscall_handler(struct intr_frame *);
 
 struct lock fs_lock;
 
-struct list open_files; 
+struct list open_files;
 
 struct file_descriptor
 {
@@ -28,16 +29,15 @@ struct file_descriptor
   struct list_elem elem;
 };
 
-static struct file_descriptor *get_open_file (int);
+static struct file_descriptor *get_open_file(int);
 
 static uint32_t *esp;
-
-
 
 bool is_valid_ptr(const void *usr_ptr)
 {
   struct thread *cur = thread_current();
-  if (usr_ptr == NULL || !is_user_vaddr(usr_ptr)) {
+  if (usr_ptr == NULL || !is_user_vaddr(usr_ptr))
+  {
     return false;
   }
   void *uaddr = pagedir_get_page(cur->pagedir, usr_ptr);
@@ -47,8 +47,8 @@ bool is_valid_ptr(const void *usr_ptr)
 void syscall_init(void)
 {
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
-  list_init (&open_files);
-  lock_init (&fs_lock);
+  list_init(&open_files);
+  lock_init(&fs_lock);
 }
 
 static void
@@ -66,6 +66,7 @@ syscall_handler(struct intr_frame *f UNUSED)
   {
     // printf("exit!\n");
     int status = *(esp + 1);
+    // printf(status);
     exit(status);
     break;
   }
@@ -73,29 +74,25 @@ syscall_handler(struct intr_frame *f UNUSED)
     halt();
     break;
 
-  
-  // case SYS_WRITE:
-  // // printf("write!\n");
-  //  f->eax = write (*(esp + 1), (void *) *(esp + 2), *(esp + 3));
-  //  break;
+  case SYS_WRITE:
+    // printf("write!\n");
+    f->eax = write(*(esp + 1), (void *)*(esp + 2), *(esp + 3));
+    break;
 
   case SYS_EXEC:
-  printf("execute!\n");
-  f->eax = exec ((char *) *(esp + 1));
-  break;
+    printf("execute!\n");
+    f->eax = exec((char *)*(esp + 1));
+    break;
 
   default:
-  // printf("%dno system call!\n", *esp);
+    printf("%dno system call!\n", *esp);
     break;
   }
-
-  
 
   // thread_exit ();
 }
 
-pid_t
-exec(const char *cmd_line)
+pid_t exec(const char *cmd_line)
 {
   struct thread *cur = thread_current();
   tid_t tid = NULL;
@@ -119,7 +116,8 @@ exec(const char *cmd_line)
   return tid;
 }
 
-int wait(pid_t pid){
+int wait(pid_t pid)
+{
   return process_wait(pid);
 }
 
@@ -129,7 +127,7 @@ void exit(int status)
   struct thread *parent = thread_get_by_id(&cur->parent_id);
 
   // parent thread가 존재할 경우, current thread를 찾기 위해 children list를 검색한다.
-  if (parent)
+  if (parent != NULL)
   {
     struct list_elem *e;
     struct child_status *child = NULL;
@@ -147,8 +145,8 @@ void exit(int status)
         break;
       }
     }
-    thread_exit();
   }
+  thread_exit();
 }
 
 void halt(void)
@@ -156,10 +154,9 @@ void halt(void)
   shutdown_power_off();
 }
 
-int
-write (int fd, const void *buffer, unsigned size)
+int write(int fd, const void *buffer, unsigned size)
 {
-  struct file_descriptor *fd_struct;  
+  struct file_descriptor *fd_struct;
   int status = 0;
 
   unsigned buffer_size = size;
@@ -167,67 +164,67 @@ write (int fd, const void *buffer, unsigned size)
 
   /* check the user memory pointing by buffer are valid */
   while (buffer_tmp != NULL)
+  {
+    if (!is_valid_ptr(buffer_tmp))
     {
-      if (!is_valid_ptr (buffer_tmp)){
-	exit (-1);
-      }
-      
-      /* Advance */ 
-      if (buffer_size > PGSIZE)
-	{
-	  buffer_tmp += PGSIZE;
-	  buffer_size -= PGSIZE;
-	}
-      else if (buffer_size == 0)
-	{
-	  /* terminate the checking loop 지금 여기있음 */
-	  buffer_tmp = NULL;
-	}
-      else
-	{
-	  /* last loop */
-	  buffer_tmp = buffer + size - 1;
-	  buffer_size = 0;
-	}
+      exit(-1);
     }
-  lock_acquire (&fs_lock); 
+
+    /* Advance */
+    if (buffer_size > PGSIZE)
+    {
+      buffer_tmp += PGSIZE;
+      buffer_size -= PGSIZE;
+    }
+    else if (buffer_size == 0)
+    {
+      /* terminate the checking loop 지금 여기있음 */
+      buffer_tmp = NULL;
+    }
+    else
+    {
+      /* last loop */
+      buffer_tmp = buffer + size - 1;
+      buffer_size = 0;
+    }
+  }
+  lock_acquire(&fs_lock);
   if (fd == 0)
+  {
+
+    status = -1;
+  }
+  else if (fd == 1)
+  {
+    putbuf(buffer, size);
+    ;
+    status = size;
+  }
+  else
+  {
+    // 지금 여기//
+    fd_struct = get_open_file(fd);
+    if (fd_struct != NULL)
     {
-      
-      status = -1;
+      status = file_write(fd_struct->file_struct, buffer, size);
     }
-  else if (fd == 0)
-    {
-      
-      putbuf (buffer, size);;
-      status = size;
-    }
-  else 
-    {
-      //지금 여기//
-      fd_struct = get_open_file (fd);
-      if (fd_struct != NULL){
-	status = file_write (fd_struct->file_struct, buffer, size);
-  printf("wowow");
-      }
-    }
-  lock_release (&fs_lock);
+  }
+  lock_release(&fs_lock);
   return status;
 }
 
 struct file_descriptor *
-get_open_file (int fd)
+get_open_file(int fd)
 {
-  
+
   struct list_elem *e;
-  struct file_descriptor *fd_struct; 
-  e = list_tail (&open_files);
-  while ((e = list_prev (e)) != list_head (&open_files)) 
-    {
-      printf("work?");
-      fd_struct = list_entry (e, struct file_descriptor, elem);
-      if (fd_struct->fd_num == fd)
-	return fd_struct;
-    }
+  struct file_descriptor *fd_struct;
+  e = list_tail(&open_files);
+  while ((e = list_prev(e)) != list_head(&open_files))
+  {
+    fd_struct = list_entry(e, struct file_descriptor, elem);
+    if (fd_struct->fd_num == fd)
+      return fd_struct;
+  }
   return NULL;
 }
